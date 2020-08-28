f5c3cb66f95dc45b31f075a7b4ce36a5c734c068
Anthony Cardona Mairena <keilorcardona0912@outlook.com>
2020-08-27 22:51:53 -0600

    the MVC structure was added to the project, also the login and signup was created

---
+++ ShoppingCart/ShoppingCartEntities/EUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartEntities
+{
+    public class EUser
+    {
+        public string Name { set; get; }
+        public string Lastname { set; get; }
+        public string Birthday { set; get; }
+        public string Country { set; get; }
+        public string State { set; get; }
+        public string Place { set; get; }
+        public int Postal { set; get; }
+        public string Direction_1 { set; get; }
+        public string Direction_2 { set; get; }
+        public string Email { set; get; }
+        public string Password { set; get; }
+    }
+}
--- ShoppingCart/ShoppingCartGUI/FrmSignUp.Designer.cs
+++ ShoppingCart/ShoppingCartGUI/FrmSignUp.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShoppingCartGUI
 {
-    partial class FrmLogin
+    partial class FrmSignUp
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSignUp));
             this.panelMain = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRepeatPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.linkLabelSignUp = new System.Windows.Forms.LinkLabel();
+            this.linkLabelLogin = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panelTop = new System.Windows.Forms.Panel();
             this.btnMin = new System.Windows.Forms.Button();
@@ -61,29 +67,89 @@
             this.panelMain.Location = new System.Drawing.Point(0, 0);
             this.panelMain.Name = "panelMain";
             this.panelMain.Size = new System.Drawing.Size(800, 450);
-            this.panelMain.TabIndex = 0;
+            this.panelMain.TabIndex = 2;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Location = new System.Drawing.Point(273, 27);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(524, 420);
+            this.panel2.Size = new System.Drawing.Size(524, 423);
             this.panel2.TabIndex = 3;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.txtRepeatPassword);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.txtPassword);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.txtEmail);
             this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.label3);
             this.panel3.Controls.Add(this.panel4);
-            this.panel3.Controls.Add(this.btnLogin);
-            this.panel3.Controls.Add(this.txtPassword);
-            this.panel3.Controls.Add(this.txtEmail);
+            this.panel3.Controls.Add(this.btnSignUp);
+            this.panel3.Controls.Add(this.txtLastname);
+            this.panel3.Controls.Add(this.txtName);
             this.panel3.Controls.Add(this.label1);
-            this.panel3.Location = new System.Drawing.Point(126, 17);
+            this.panel3.Location = new System.Drawing.Point(126, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(282, 366);
+            this.panel3.Size = new System.Drawing.Size(282, 417);
             this.panel3.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(36, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Repetir contraseña:";
+            // 
+            // txtRepeatPassword
+            // 
+            this.txtRepeatPassword.Location = new System.Drawing.Point(39, 265);
+            this.txtRepeatPassword.Name = "txtRepeatPassword";
+            this.txtRepeatPassword.Size = new System.Drawing.Size(217, 20);
+            this.txtRepeatPassword.TabIndex = 11;
+            this.txtRepeatPassword.UseSystemPasswordChar = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(36, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Contraseña:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(39, 226);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(217, 20);
+            this.txtPassword.TabIndex = 9;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(36, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Correo:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(39, 186);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(217, 20);
+            this.txtEmail.TabIndex = 7;
+            this.txtEmail.UseSystemPasswordChar = true;
             // 
             // label4
             // 
@@ -91,9 +157,9 @@
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(36, 127);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.Size = new System.Drawing.Size(47, 13);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Contraseña:";
+            this.label4.Text = "Apellido:";
             // 
             // label3
             // 
@@ -101,79 +167,79 @@
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.Location = new System.Drawing.Point(36, 85);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.Size = new System.Drawing.Size(47, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Correo:";
+            this.label3.Text = "Nombre:";
             // 
             // panel4
             // 
-            this.panel4.Controls.Add(this.linkLabelSignUp);
+            this.panel4.Controls.Add(this.linkLabelLogin);
             this.panel4.Controls.Add(this.label2);
-            this.panel4.Location = new System.Drawing.Point(39, 230);
+            this.panel4.Location = new System.Drawing.Point(0, 335);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(217, 70);
+            this.panel4.Size = new System.Drawing.Size(282, 70);
             this.panel4.TabIndex = 4;
             // 
-            // linkLabelSignUp
+            // linkLabelLogin
             // 
-            this.linkLabelSignUp.AutoSize = true;
-            this.linkLabelSignUp.LinkColor = System.Drawing.Color.White;
-            this.linkLabelSignUp.Location = new System.Drawing.Point(79, 41);
-            this.linkLabelSignUp.Name = "linkLabelSignUp";
-            this.linkLabelSignUp.Size = new System.Drawing.Size(63, 13);
-            this.linkLabelSignUp.TabIndex = 1;
-            this.linkLabelSignUp.TabStop = true;
-            this.linkLabelSignUp.Text = "¡Crear aquí!";
-            this.linkLabelSignUp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSignUp_LinkClicked);
+            this.linkLabelLogin.AutoSize = true;
+            this.linkLabelLogin.LinkColor = System.Drawing.Color.White;
+            this.linkLabelLogin.Location = new System.Drawing.Point(109, 41);
+            this.linkLabelLogin.Name = "linkLabelLogin";
+            this.linkLabelLogin.Size = new System.Drawing.Size(73, 13);
+            this.linkLabelLogin.TabIndex = 1;
+            this.linkLabelLogin.TabStop = true;
+            this.linkLabelLogin.Text = "Iniciar session";
+            this.linkLabelLogin.MouseClick += new System.Windows.Forms.MouseEventHandler(this.linkLabelLogin_LinkClicked);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(25, 9);
+            this.label2.Location = new System.Drawing.Point(5, 15);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(167, 18);
+            this.label2.Size = new System.Drawing.Size(277, 18);
             this.label2.TabIndex = 0;
-            this.label2.Text = "¿No tienes una cuenta?.";
+            this.label2.Text = "Tengo una cuenta, quiero iniciar session.";
             // 
-            // btnLogin
+            // btnSignUp
             // 
-            this.btnLogin.BackColor = System.Drawing.Color.White;
-            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnLogin.Location = new System.Drawing.Point(100, 179);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(96, 27);
-            this.btnLogin.TabIndex = 3;
-            this.btnLogin.Text = "Entrar";
-            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnSignUp.BackColor = System.Drawing.Color.White;
+            this.btnSignUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignUp.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSignUp.Location = new System.Drawing.Point(100, 302);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(96, 27);
+            this.btnSignUp.TabIndex = 3;
+            this.btnSignUp.Text = "Registrarme";
+            this.btnSignUp.UseVisualStyleBackColor = false;
             // 
-            // txtPassword
+            // txtLastname
             // 
-            this.txtPassword.Location = new System.Drawing.Point(39, 143);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(217, 20);
-            this.txtPassword.TabIndex = 2;
-            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtLastname.Location = new System.Drawing.Point(39, 143);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(217, 20);
+            this.txtLastname.TabIndex = 2;
+            this.txtLastname.UseSystemPasswordChar = true;
             // 
-            // txtEmail
+            // txtName
             // 
-            this.txtEmail.Location = new System.Drawing.Point(39, 101);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(217, 20);
-            this.txtEmail.TabIndex = 1;
+            this.txtName.Location = new System.Drawing.Point(39, 101);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(217, 20);
+            this.txtName.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(52, 25);
+            this.label1.Location = new System.Drawing.Point(94, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 31);
+            this.label1.Size = new System.Drawing.Size(116, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Iniciar Session";
+            this.label1.Text = "Registro";
             // 
             // panelTop
             // 
@@ -200,7 +266,7 @@
             this.btnMin.Size = new System.Drawing.Size(17, 16);
             this.btnMin.TabIndex = 1;
             this.btnMin.UseVisualStyleBackColor = false;
-            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            this.btnMin.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnMin_Click);
             // 
             // btnClose
             // 
@@ -215,7 +281,7 @@
             this.btnClose.Size = new System.Drawing.Size(17, 16);
             this.btnClose.TabIndex = 0;
             this.btnClose.UseVisualStyleBackColor = false;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            this.btnClose.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnClose_Click);
             // 
             // panel1
             // 
@@ -227,17 +293,17 @@
             this.panel1.Size = new System.Drawing.Size(267, 450);
             this.panel1.TabIndex = 1;
             // 
-            // FrmLogin
+            // FrmSignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panelMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "FrmLogin";
+            this.Name = "FrmSignUp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FrmLogin";
+            this.Text = "FrmSignUp";
             this.panelMain.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
@@ -252,20 +318,26 @@
         #endregion
 
         private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtRepeatPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.LinkLabel linkLabelLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panelTop;
         private System.Windows.Forms.Button btnMin;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.LinkLabel linkLabelSignUp;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
     }
 }
